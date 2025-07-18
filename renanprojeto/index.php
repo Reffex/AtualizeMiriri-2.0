5210a55b9275632f4cf6e649575a6854da6b6f57
Renan Filipe <renanf0705@gmail.com>
2025-07-18 15:16:59 -0300

    Update index.php

--- renanprojeto/index.php
+++ renanprojeto/index.php
@@ -8,13 +8,13 @@ require_once 'includes/auto_check.php';
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Início</title>
   <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet">
   <link rel="stylesheet" href="assets/css/styles.css">
+  <title>Atualize Miriri</title>
 </head>
 
 <body>
-  <div class="form-box">
+  <div>
     <h1 class="text-center">Bem-Vindo, <?= $_SESSION['usuario_nome'] ?? 'Usuário' ?>!</h1>
     <div class="menu-row">
       <a href="pages/clientes/listar.php" class="menu-link">
