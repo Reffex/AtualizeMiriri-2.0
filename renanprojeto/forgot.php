ac10ed31c53b3b16626364157ddaaf28a9fb8fda
Renan Filipe <renanf0705@gmail.com>
2025-07-18 15:00:08 -0300

    Create forgot.php

---
+++ renanprojeto/forgot.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+  <link rel="stylesheet" href="./assets/css/styles.css">
+  <title>Recuperar Senha - Atualize Miriri</title>
+</head>
+
+<body>
+  <main class="container">
+    <form>
+      <h1>Recuperar Senha</h1>
+
+      <div class="input-box">
+        <input type="email" placeholder="Digite seu e-mail" required>
+        <i class='bx bxs-envelope'></i>
+      </div>
+
+      <button type="submit" class="login">Enviar</button>
+
+      <div class="register-link">
+        <p>Lembrou sua senha? <a href="login.php">Voltar ao Login</a></p>
+      </div>
+    </form>
+  </main>
+</body>
+
+</html>
